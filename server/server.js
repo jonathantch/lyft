b79d76d0df22ed07cf77d41f6d95dbf82de08142
Jonathan Hsieh <jonathan@Jons-MacBook-Pro.local>
2020-08-01 17:50:23 -0700

    implement the /test post route

--- server/server.js
+++ server/server.js
@@ -6,6 +6,19 @@ const port = 3000;
 app.use(bodyParser.json());
 
 app.post('/test', (req, res) => {
+  const input = req.body.string_to_cut;
+  const outputArr = [];
+
+  if (typeof input !== 'string') {
+    res.status(422).send('The input is not valid.');
+  } else {
+    for (let i = 0; i < input.length; i++) {
+      if ((i + 1) % 3 === 0) {
+        outputArr.push(input[i]);
+      }
+    }
+    res.status(200).send({return_string: outputArr.join('')});
+  }
 });
 
 app.listen(port, () => {
